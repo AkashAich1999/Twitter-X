e5bf1167885dec079a3703b06c960db7edf7251c
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-05-22 20:49:37 +0530

    useFollow hook added

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -32,7 +32,7 @@ export const followUnfollowUser = async (req, res) => {
             await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
             await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
             res.status(200).json({ message:"User Unfollowed Successfully." })
-            // TODO: RETURN THE ID OF THE USER AS RESPONSE
+            
         } else {
             // follow the user
             await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
@@ -46,7 +46,6 @@ export const followUnfollowUser = async (req, res) => {
 
             await newNotification.save();
 
-            // TODO: RETURN THE ID OF THE USER AS RESPONSE
             res.status(200).json({ message:"User Followed Successfully." })
         }
 
